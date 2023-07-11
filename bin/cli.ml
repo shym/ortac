c29bd915d1d21a02ff93ed2460205524f6445df1
Nicolas Osborne <nicolas.osborne@tarides.com>
2023-07-11 17:25:42 +0200

    add an error message when no plugins are installed

--- bin/cli.ml
+++ bin/cli.ml
@@ -2,13 +2,19 @@ let () = Sites.Plugins.Plugins.load_all ()
 
 open Cmdliner
 
-let group =
-  let cmds =
-    Registration.fold (fun acc cmd -> cmd :: acc) [] Registration.plugins
-  in
-  let doc = "Run ORTAC." in
-  let version = "ortac version %%VERSION%%" in
-  let info = Cmd.info "ortac" ~doc ~version in
-  Cmd.group info cmds
+let usage () =
+  Format.(fprintf std_formatter)
+    "ortac command-line tool requires at least one plugin.@\n\
+     Please install one."
 
-let () = Stdlib.exit (Cmd.eval group)
+let () =
+  match
+    Registration.fold (fun acc cmd -> cmd :: acc) [] Registration.plugins
+  with
+  | [] -> usage ()
+  | cmds ->
+      let doc = "Run ORTAC." in
+      let version = "ortac version %%VERSION%%" in
+      let info = Cmd.info "ortac" ~doc ~version in
+      let group = Cmd.group info cmds in
+      Stdlib.exit (Cmd.eval group)
