b367bfd2195a9d17abb3c747fa07cc4f139bcaba
Samuel Hym <samuel.hym@rustyne.lautre.net>
2024-04-12 16:03:27 +0200

    squash! Use default `Cmdliner.Term` rather than `usage` function
    Define a default Cmd to handle missing command
    
    Correctly handle `--help` and `--version` without any command
    Improve the error message when no command is given, with a different
    error message whether at least one plugin is available or not

--- bin/cli.ml
+++ bin/cli.ml
@@ -5,6 +5,19 @@ let () =
 
 open Cmdliner
 
+let default_cmd cmds =
+  let errmsg, usage =
+    match cmds with
+    | [] ->
+        (* We cannot be in the case where ORTAC_ONLY_PLUGIN was set, since
+           trying to load only one missing plugin would have raised an exception
+           earlier, so we know no plugin is available *)
+        (* Displaying usage in that case is useless *)
+        ("error: no plugin is available, please install at least one", false)
+    | _ -> ("error: missing command", true)
+  in
+  Term.(term_result ~usage (const (Result.error (`Msg errmsg))))
+
 let () =
   let doc = "Run ORTAC."
   and version =
@@ -14,9 +27,9 @@ let () =
       | Some v -> Build_info.V1.Version.to_string v)
   in
   let info = Cmd.info "ortac" ~doc ~version
-  and default = Term.(ret (const (`Help (`Pager, None))))
   and cmds =
     Registration.fold (fun acc cmd -> cmd :: acc) [] Registration.plugins
   in
+  let default = default_cmd cmds in
   let group = Cmd.group info ~default cmds in
   Stdlib.exit (Cmd.eval group)
